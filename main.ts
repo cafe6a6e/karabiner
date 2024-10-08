240cb9fe903957aa0b63a2814d0e0f40b500d662
CafebabeTimeLapse <taka1stdec@gmail.com>
2024-10-08 12:13:55 +0900

    .

--- main.ts
+++ main.ts
@@ -18,52 +18,12 @@ if (!profileName) {
 }
 
 const capsLockLayerVarName = "layer-caps_lock";
-const fKeyLayerVarName = "layer-f";
-const gKeyLayerVarName = "layer-g";
 
 writeToProfile(profileName, [
-  duoLayer("f", "⇪", fKeyLayerVarName)
-    .notification("⇪f-layer: num+⇪ symbols and =_")
-    .manipulators([
-      map("m").to("1", "shift"),
-      map(",").to("2", "shift"),
-      map(".").to("3", "shift"),
-      map("j").to("4", "shift"),
-      map("k").to("5", "shift"),
-      map("l").to("6", "shift"),
-      map("u").to("7", "shift"),
-      map("i").to("8", "shift"),
-      map("o").to("9", "shift"),
-      map("p").to("0", "shift"),
-      map("/").to("="),
-      map(";").to("-", "shift"),
-    ]),
-  duoLayer("g", "⇪", gKeyLayerVarName)
-    .notification("⇪g-layer: num and +-")
-    .manipulators([
-      map("␣").to("0"),
-      map("m").to("1"),
-      map(",").to("2"),
-      map(".").to("3"),
-      map("j").to("4"),
-      map("k").to("5"),
-      map("l").to("6"),
-      map("u").to("7"),
-      map("i").to("8"),
-      map("o").to("9"),
-      map("p").to("0"),
-      map("/").to("=", "shift"),
-      map(";").to("-"),
-      map("p").to("8", "shift"),
-      map("right_command").to("."),
-    ]),
   layer("⇪", capsLockLayerVarName)
-    .notification("⇪-Layer: ←↓↑→ ()[]{} `| -= _+ \\~ ")
     .configKey((v) => v.toIfAlone("[", ["control"]), true) // ESC alternative in VIM
-    .modifiers("??")
     .manipulators([
-      map("q").to("`", "shift"),
-      map("a").to("\\"),
+      // right hand brackets and vim cursor
       map("y").to("`"),
       map("u").to("[", "shift"),
       map("i").to("]", "shift"),
@@ -83,8 +43,30 @@ writeToProfile(profileName, [
       mapPointingButton("button1").to("←", "command"),
       mapPointingButton("button2").to("→", "command"),
       mapPointingButton("button3").to("mission_control"),
-      map("f").toVar(fKeyLayerVarName, 1, 0),
-      map("g").toVar(gKeyLayerVarName, 1, 0),
+      // left hand symbols
+      map("q").to("`", "shift"),
+      map("a").to("\\"),
+      map("x").to("1", "shift"),
+      map("c").to("2", "shift"),
+      map("v").to("3", "shift"),
+      map("s").to("4", "shift"),
+      map("d").to("5", "shift"),
+      map("f").to("6", "shift"),
+      map("w").to("7", "shift"),
+      map("e").to("8", "shift"),
+      map("r").to("9", "shift"),
+      map("␣").to("0", "shift"),
+      // left hand numbers
+      map("x", "shift").to("1"),
+      map("c", "shift").to("2"),
+      map("v", "shift").to("3"),
+      map("s", "shift").to("4"),
+      map("d", "shift").to("5"),
+      map("f", "shift").to("6"),
+      map("w", "shift").to("7"),
+      map("e", "shift").to("8"),
+      map("r", "shift").to("9"),
+      map("␣", "shift").to("0"),
     ]),
   rule("Trackball mouse motion").manipulators([
     mouseMotionToScroll()
