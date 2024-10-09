0fb6171b05088ef704feea37faa6678f2d510121
CafebabeTimeLapse <taka1stdec@gmail.com>
2024-10-09 10:26:29 +0900

    .

--- main.ts
+++ main.ts
@@ -18,13 +18,12 @@ if (!profileName) {
 }
 
 const capsLockLayerVarName = "layer-caps_lock";
-const fKeyLayerVarName = "layer-f";
-const gKeyLayerVarName = "layer-g";
+const tabLayerVarName = "layer-tab";
 
 writeToProfile(profileName, [
-  duoLayer("f", "⇪", fKeyLayerVarName)
-    .notification("⇪f-layer: num+⇪ symbols and =_")
+  layer("tab", tabLayerVarName)
     .manipulators([
+      // symbols
       map("m").to("1", "shift"),
       map(",").to("2", "shift"),
       map(".").to("3", "shift"),
@@ -35,40 +34,32 @@ writeToProfile(profileName, [
       map("i").to("8", "shift"),
       map("o").to("9", "shift"),
       map("p").to("0", "shift"),
-      map("/").to("="),
-      map(";").to("-", "shift"),
-    ]),
-  duoLayer("g", "⇪", gKeyLayerVarName)
-    .notification("⇪g-layer: num and +-")
-    .manipulators([
-      map("␣").to("0"),
-      map("m").to("1"),
-      map(",").to("2"),
-      map(".").to("3"),
-      map("j").to("4"),
-      map("k").to("5"),
-      map("l").to("6"),
-      map("u").to("7"),
-      map("i").to("8"),
-      map("o").to("9"),
-      map("p").to("0"),
-      map("/").to("=", "shift"),
-      map(";").to("-"),
-      map("p").to("8", "shift"),
-      map("right_command").to("."),
+
+      // numbers
+      map("␣", "shift").to("0"),
+      map("m", "shift").to("1"),
+      map(",", "shift").to("2"),
+      map(".", "shift").to("3"),
+      map("j", "shift").to("4"),
+      map("k", "shift").to("5"),
+      map("l", "shift").to("6"),
+      map("u", "shift").to("7"),
+      map("i", "shift").to("8"),
+      map("o", "shift").to("9"),
+      map("/", "shift").to("."),
     ]),
   layer("⇪", capsLockLayerVarName)
     .notification("⇪-Layer: ←↓↑→ ()[]{} `| -= _+ \\~ ")
     .configKey((v) => v.toIfAlone("[", ["control"]), true) // ESC alternative in VIM
     .modifiers("??")
     .manipulators([
-      map("q").to("`", "shift"),
-      map("a").to("\\"),
       map("y").to("`"),
       map("u").to("[", "shift"),
       map("i").to("]", "shift"),
       map("o").to("9", "shift"),
       map("p").to("0", "shift"),
+      map("[").to("`", "shift"),
+      map("]").to("\\"),
       map("h").to("←"),
       map("j").to("↓"),
       map("k").to("↑"),
@@ -83,8 +74,6 @@ writeToProfile(profileName, [
       mapPointingButton("button1").to("←", "command"),
       mapPointingButton("button2").to("→", "command"),
       mapPointingButton("button3").to("mission_control"),
-      map("f").toVar(fKeyLayerVarName, 1, 0),
-      map("g").toVar(gKeyLayerVarName, 1, 0),
     ]),
   rule("Trackball mouse motion").manipulators([
     mouseMotionToScroll()
