0672a99c559df56fc5c0684aa9a063907e714082
CafebabeTimeLapse <taka1stdec@gmail.com>
2025-04-18 14:20:56 +0900

    .

--- main.ts
+++ main.ts
@@ -33,23 +33,6 @@ writeToProfile(profileName, [
     map("p").to("0", "shift"),
     map("␣").to("␣"),
   ]),
-  layer("tab", tabLayerVarName)
-    .modifiers(["shift"])
-    .configKey((v) => v.toIfAlone("tab", "shift"))
-    .manipulators([
-      // map("m").to("1"),
-      // map(",").to("2"),
-      // map(".").to("3"),
-      // map("j").to("4"),
-      // map("k").to("5"),
-      // map("l").to("6"),
-      // map("u").to("7"),
-      // map("i").to("8"),
-      // map("o").to("9"),
-      // map("p").to("0"),
-      // map("␣").to("0"),
-      // map("/").to("."),
-    ]),
   layer("⇪", capsLockLayerVarName)
     // .configKey((v) => v.toIfAlone("[", ["control"]), true) // ESC alternative in VIM
     .configKey((v) => v.toIfAlone("escape"), true)
@@ -104,5 +87,16 @@ writeToProfile(profileName, [
     map("p", "l⌃").to("=", "shift"),
     map("l⌃").toIfAlone("␣", "l⌃").to("l⌘"),
     map("l⌘").to("l⌃"),
+    map("h", "⌥").toMouseKey({ x: -1536 }),
+    map("j", "⌥").toMouseKey({ y: 1536 }),
+    map("k", "⌥").toMouseKey({ y: -1536 }),
+    map("l", "⌥").toMouseKey({ x: 1536 }),
+    map("h", "⌥", "shift").toMouseKey({ horizontal_wheel: -36 }),
+    map("j", "⌥", "shift").toMouseKey({ vertical_wheel: 36 }),
+    map("k", "⌥", "shift").toMouseKey({ vertical_wheel: -36 }),
+    map("l", "⌥", "shift").toMouseKey({ horizontal_wheel: 36 }),
+    map("u", "⌥").toPointingButton("button1"),
+    map("i", "⌥").toPointingButton("button2"),
+    map("u", "⌥", "shift").toPointingButton("button3"),
   ]),
 ]);
